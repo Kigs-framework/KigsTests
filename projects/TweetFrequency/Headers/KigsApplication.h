a4b731bbc9c7abfd285ab8a3ac4d96de1910ec42
Stephane <stephane.capo@next-bim.com>
2022-12-21 11:09:45 +0100

    use same data folder than others

--- projects/TweetFrequency/Headers/KigsApplication.h
+++ projects/TweetFrequency/Headers/KigsApplication.h
@@ -9,7 +9,7 @@
 
 #ifdef INIT_DEFAULT_MODULES
 //! And then I want the base data path to be :
-#define BASE_DATA_PATH "assetsRT"
+#define BASE_DATA_PATH "assets"
 #endif //INIT_DEFAULT_MODULES
 
 //! then define it as the application class 
